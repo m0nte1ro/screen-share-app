9fe67386b0247df374bbfc7560452ffa42e3e1b1
unknown <ricardo.monteiro412@gmail.com>
2022-03-28 23:18:43 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 
+async function GetScreen(){
+  const video = document.getElementById('video');
+  if(video.srcObject==null){
+    await window.navigator.mediaDevices.getDisplayMedia(
+      { 
+        video: true, 
+        audio: true 
+      }
+    ).then(
+      stream => {video.srcObject = stream}, 
+      err=> console.log(err)
+    );
+    video.play();
+  } 
+  else {
+    video.pause();
+    video.srcObject=null;
+    video.load();
+  }
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> Screen Sharing App!</h1>
+        <video id="video" autoPlay></video>
+        <div id="start">
+          {/* <input id="code-input" placeholder="Enter your code"/> */}
+          <button id="start-button" onClick={()=>GetScreen()}>Start Screen Sharing</button>
+        </div>
       </header>
     </div>
   );
